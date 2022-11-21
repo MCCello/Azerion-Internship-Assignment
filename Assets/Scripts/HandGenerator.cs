f5020ee1c750975dd42e292911e912ad5566bfb7
MCCello <86051935+MCCello@users.noreply.github.com>
2022-11-21 15:59:41 +0100

    Hand generator

---
+++ Assets/Scripts/HandGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HandGenerator : MonoBehaviour
+{
+    public InitializeCards initializeCards;
+
+    private void Start()
+    {
+        initializeCards = GetComponent<InitializeCards>();
+    }
+
+    public void GetHand()
+    {
+        CreateHand();
+    }
+    //Randomizing the number to get element from list makes it possible to have repeated cards as shown in the example
+    //additionally it is more efficient as we are referencing the cards in a centralized startup location.
+    private List<Card> CreateHand()
+    {
+        List<Card> thisHand = new List<Card>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            int random = Random.Range(0, initializeCards.Deck.Count);
+
+            thisHand.Add(initializeCards.Deck.ElementAt(random));
+
+            Debug.Log(thisHand[i].ToString());
+        }
+        
+        return thisHand;
+    }
+}
