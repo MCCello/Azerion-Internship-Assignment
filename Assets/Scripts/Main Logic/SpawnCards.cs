49857cc8ed1daf8f694db48cfc634915148d29db
MCCello <86051935+MCCello@users.noreply.github.com>
2022-11-22 18:57:04 +0100

    Visual Aspects for the game -> Description
    Added prefabs for each card with a custom script used for identifying the card
    added a button to show the main players hand
    Next step -> add visual representation for "AI players"

---
+++ Assets/Scripts/Main Logic/SpawnCards.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnCards : MonoBehaviour
+{
+    public Player player;
+    public List<ScriptableCard> cards;
+    List<Image> spriteCards;
+
+    public void ShowHand()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        spriteCards = GetComponentsInChildren<Image>().ToList();
+
+        for (int i = 0; i < player.hand.Count; i++)
+        {
+            ScriptableCard c = cards.Find(x => x.Number == player.hand[i].Number);
+
+            spriteCards[i].sprite = c.GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+}
